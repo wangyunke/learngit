b93a25fb65a80b962d1570d87ec6f4da5defb41e
wangyunke <awangyunke@163.com>
2015-11-27 01:00:48 +0800

    add

--- app/src/main/java/com/example/ykw/mygit/MainActivity.java
+++ app/src/main/java/com/example/ykw/mygit/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        System.out.println("我成功了");
     }
 
     @Override
